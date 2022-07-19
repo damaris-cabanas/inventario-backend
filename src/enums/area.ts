a082437e9a8d08654d015fb3304b20a78b6e4338
damaristorres_ <damaris.cabanas.98@gmail.com>
2022-07-19 15:55:02 -0400

    PD - [122]

---
+++ src/enums/area.ts
@@ -0,0 +1,11 @@
+export enum Area {
+    FINANCIERA = 'Financiera',
+    TAC = 'TAC',
+    TIC = 'TIC',
+    COMPRAS = 'Compras',
+    TSI = 'TSI',
+    GOBERNANZA = 'GOB',
+    TESORERIA = 'Tesoreria',
+    JURIDICA = 'Juridica',
+  }
+  
